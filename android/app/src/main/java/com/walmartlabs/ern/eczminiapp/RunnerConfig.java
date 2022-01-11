de6ab86913e73ad1c26aeddb7c3466dacc288c16
Zafer Bozkurt <zbzkurt@gmail.com>
2022-01-11 18:08:21 +0300

    add android runner

---
+++ android/app/src/main/java/com/walmartlabs/ern/eczminiapp/RunnerConfig.java
@@ -0,0 +1,17 @@
+package com.walmartlabs.ern.eczminiapp;
+
+import com.walmartlabs.ern.container.miniapps.EczMiniappActivity;
+
+//
+// GENERATED CODE: DO NOT MODIFY
+//
+// Do not modify the content of this file as it will be regenerated
+// every time a run-android command is executed.
+// See https://native.electrode.io/cli-commands/run-android
+
+final class RunnerConfig {
+    static final Class MAIN_MINIAPP_ACTIVITY_CLASS = EczMiniappActivity.class;
+    static final boolean RN_DEV_SUPPORT_ENABLED = true;
+    static final String PACKAGER_HOST = "localhost";
+    static final String PACKAGER_PORT = "8081";
+}
